64d7535dbe232593df5ccd815973ee9d67b1be91
eisnerh <elopeza@ucatolica.ac.cr>
2016-10-07 22:03:58 -0600

    Buscar, Eliminar
    Clase para el adaptador de los Cursores y llenado del listview

--- MyApplication/app/src/main/java/com/so2/ace/agenda2017/Buscar.java
+++ MyApplication/app/src/main/java/com/so2/ace/agenda2017/Buscar.java
@@ -1,13 +1,29 @@
 package com.so2.ace.agenda2017;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class Buscar extends AppCompatActivity {
+
+    EditText inputSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar);
+
+        MyDBHandler dbHandler;
+        dbHandler = new MyDBHandler(this, null, null, 1);
+        SQLiteDatabase db = dbHandler.getWritableDatabase();
+        Cursor cursor = dbHandler.listarpersonas();
+
+        ListView lvl_items = (ListView) findViewById(R.id.lvl_items);
+        lvl_items.setTextFilterEnabled(true);
+        final TodoCursorAdapter todoAdapter = new TodoCursorAdapter(this, cursor);
+        lvl_items.setAdapter(todoAdapter);
     }
 }
--- MyApplication/app/src/main/java/com/so2/ace/agenda2017/Eliminar.java
+++ MyApplication/app/src/main/java/com/so2/ace/agenda2017/Eliminar.java
@@ -1,13 +1,46 @@
 package com.so2.ace.agenda2017;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Eliminar extends AppCompatActivity {
+
+    MyDBHandler dbHandler;
+    EditText elimina_input;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eliminar);
+        elimina_input = (EditText) findViewById(R.id.modificar_input);
+        dbHandler = new MyDBHandler(this, null, null, 1);
+    }
+
+    public void eliminar_clicked(View view){
+
+        dbHandler.borrarPersona(Integer.parseInt(elimina_input.getText().toString()));
+        confirmacion();
+
+
+    }
+
+    public void confirmacion(){
+
+        AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+        dlgAlert.setMessage(elimina_input.getText());
+        dlgAlert.setTitle("Se ha eliminado satisfactoriamente!!!");
+        dlgAlert.setPositiveButton("Ok",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //dismiss the dialog
+                    }
+                });
+        dlgAlert.setCancelable(true);
+        dlgAlert.create().show();
     }
 }
