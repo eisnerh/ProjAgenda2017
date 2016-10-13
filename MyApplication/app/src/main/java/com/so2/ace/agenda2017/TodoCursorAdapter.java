a33353aba7fff4ce1f493a4f1f2a22218721ac43
eisnerh <elopeza@ucatolica.ac.cr>
2016-10-12 22:15:48 -0600

    Buscar

--- MyApplication/app/src/main/java/com/so2/ace/agenda2017/TodoCursorAdapter.java
+++ MyApplication/app/src/main/java/com/so2/ace/agenda2017/TodoCursorAdapter.java
@@ -1,6 +1,12 @@
 package com.so2.ace.agenda2017;
 
-import android.support.v4.widget.CursorAdapter;
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
 
 /**
  * Created by eisne on 3/10/2016.
@@ -14,7 +20,7 @@ public class TodoCursorAdapter extends CursorAdapter {
     // you don't bind any data to the view at this point.
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.activity_buscar_persona, parent, false);
+        return LayoutInflater.from(context).inflate(R.layout.activity_buscar, parent, false);
     }
 
     // The bindView method is used to bind all data to a given view
@@ -22,20 +28,20 @@ public class TodoCursorAdapter extends CursorAdapter {
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
 
-        TextView idtxt = (TextView) view.findViewById(R.id.id_txt);
-        TextView apellidotxt = (TextView) view.findViewById(R.id.apellido_txt);
-        TextView nombretxt = (TextView) view.findViewById(R.id.nombre_txt);
+        TextView id_txt = (TextView) view.findViewById(R.id.id_txt);
+        TextView apellido_txt = (TextView) view.findViewById(R.id.apellido_txt);
+        TextView nombre_txt = (TextView) view.findViewById(R.id.nombre_txt);
 
-        // Extraccion de las propiedades del cursor
+        // Extracción de las propiedades del cursor
 
 
-        int txtid = cursor.getInt(cursor.getColumnIndexOrThrow("_id"));
-        String txtapellido = cursor.getString(cursor.getColumnIndexOrThrow("apellido"));
-        String txtnombre = cursor.getString(cursor.getColumnIndexOrThrow("nombre"));
+        int txt_id = cursor.getInt(cursor.getColumnIndexOrThrow("_id"));
+        String txt_apellido = cursor.getString(cursor.getColumnIndexOrThrow("apellido"));
+        String txt_nombre = cursor.getString(cursor.getColumnIndexOrThrow("nombre"));
 
-        idtxt.setText(String.valueOf(txtid));
-        apellidotxt.setText(txtapellido);
-        nombretxt.setText(txtnombre);
+        id_txt.setText(String.valueOf(txt_id));
+        apellido_txt.setText(txt_apellido);
+        nombre_txt.setText(txt_nombre);
 
     }
 }
